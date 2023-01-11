86eba924b8f9308441f1de95dfe90c34ab7a2fa3
Muratov <muratoff101088@gmail.com>
2023-01-11 19:57:00 +0300

    homeWork

--- src/main/java/com/example/recipes/controllers/FirstController.java
+++ src/main/java/com/example/recipes/controllers/FirstController.java
@@ -1,2 +1,19 @@
-package com.example.recipes.controllers;public class FirstController {
+package com.example.recipes.controllers;
+
+import com.example.recipes.info.Information;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+    @GetMapping
+    public String appStarting() {
+        return "Приложение запущено";
+    }
+    @GetMapping("/info")
+    public String info() {
+        return new Information().toString();
+    }
+
+
 }
